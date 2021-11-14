c242acb037aeaebd4e75baeb72dfff1d991fddbb
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-14 17:46:27 -0500

    removing boiler plate react code from App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './media/logo.svg';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Re-testing that changes show up in gh pages deployment"
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      "Hello There!"
     </div>
   );
 }
