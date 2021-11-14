b9aaa4b9cd7c70b30a3d15da13a8992940097e9e
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-14 17:59:12 -0500

    adding vanilla bootstrap to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-primary">
       "Hello There!"
     </div>
   );
