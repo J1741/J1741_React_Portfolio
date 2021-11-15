1893515a42faa4b65506f09febb3ed94d4449404
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-15 01:07:52 -0500

    adding files for header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Header.css';
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Testing Header</h1>
+    </div>
+  );
+}
