b0fe6749f5394d67f001b7c124ba86a0f138ebdb
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-15 04:44:19 -0500

    adding icons and styling to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Footer.css';
+import gitHubLogo from '../media/github_logo_white.png';
+import linkedInLogo from '../media/linkedin_logo_white.png';
+import stackOverflowLogo from '../media/stackoverflow_logo_white.png';
 
 export default function Footer() {
   return (
     <div className="footer-area">
-      <h2>hello there!</h2>  
+
+      <ul className="icon-set d-flex">
+        {/* Github */}
+        <li><a href="https://github.com/j1741" target="_blank"><img className="icon-image" src={gitHubLogo} alt="GitHub" title="GitHub"/></a></li>
+
+        {/* LinkedIn */}
+        <li><a href="https://www.linkedin.com/in/kira-griffitt-7232b56b" target="_blank"><img className="icon-image" src={linkedInLogo} alt="LinkedIn" title="LinkedIn"/></a></li>
+
+        {/* Stack Overflow */}
+        <li><a href="https://stackoverflow.com/users/16043908/a2277" target="_blank"><img className="icon-image" src={stackOverflowLogo} alt="Stack Overflow" title="Stack Overflow"/></a></li>
+      </ul>
+
     </div>
   )
 }
