44bea4b03eed55cbda4688062484797ce447c1af
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-18 03:23:09 -0500

    tweaking text styling in about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ export default function AboutMe() {
     <div className="container d-flex about-me">
       <img className="head-shot" src={headShot}/>
       <p className="container bio-text">
-        Hello there! I'm <a className="bio-link" target="blank" href="https://github.com/J1741">J1741</a>, a full stack web developer with a strong sense of design, a love for <a className="hidden-link" target="blank" href="https://gist.github.com/J1741/f1d71d52a92d725a70aa0af62df21555">technical detail</a>. 
+        Hello there! I'm <a className="bio-link" target="blank" href="https://github.com/J1741">J1741</a>, a full stack web developer with a strong sense of design and a love for <a className="hidden-link" target="blank" href="https://gist.github.com/J1741/f1d71d52a92d725a70aa0af62df21555">technical detail</a>. 
         <br /><br />
         I enjoy full stack development because it allows me to exercise my creative as well as analytical skill sets. While I excel at front end development, I am always deepening my back end development skills, and particularly enjoy the challenges of scripting and database design.
         <br /><br />
