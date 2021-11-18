6e545b3c44695226784ccf5d3dafe2580758ae15
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-18 02:33:16 -0500

    adding and styling headshot in about me section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
       <Footer />
     </div>
   );
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/AboutMe.css'
+import '../styles/AboutMe.css';
+import headShot from '../media/head_shot_square_close.png';
+
+export default function AboutMe() {
+  return (
+    <div className="container d-flex about-me">
+      <img className="logo-image" src={headShot}/>
+      <h1>Hello There!</h1>
+    </div>
+  )
+}
