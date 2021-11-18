df00f107a316dd7478800dd59e80c638cee12d57
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-18 08:39:29 -0500

    copying in additional media files; adding banner image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Header.css';
 import Navigation  from './Navigation';
-import Footer from './Footer';
+import profileBanner from '../media/profile_banner.jpeg';
 
 export default function Header() {
   return (
+    <>
     <header className="header d-flex">
       <h1>J1741</h1>
       <Navigation />
     </header>
+    <img class="banner-image" src={profileBanner} />
+    </>
   );
 }
