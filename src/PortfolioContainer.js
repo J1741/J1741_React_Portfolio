8872206e8bc98f67c5fcc011e72d8128bac9da47
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-19 00:12:07 -0500

    setting up app to render about me page by default

--- src/PortfolioContainer.js
+++ src/PortfolioContainer.js
@@ -1,13 +1,36 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/PortfolioContainer.css';
+import AboutMe from './components/AboutMe';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+// renders portfolio container
 export default function PortfolioContainer() {
+
+  // set AboutMe as default value of currentPage
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  // render page based on value of currentPage
+  const renderPage = () => {
+    // ** if currentPage is AboutMe, return AboutMe
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    // ** if currentPage is Portfolio, return Portfolio
+    // ** if currentPage is Contact, return Contact
+    // ** if currentPage is Resume, return Resume
+    // otherwise, return AboutMe
+    return <AboutMe />;
+  }
+
+  // handle page change by changing currentPage state
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <>
       <Header />
+      {renderPage()}      
       <Footer />
     </>
   );
