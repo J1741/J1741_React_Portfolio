d0f092ba433fc1bd19be2906d5b6391e63860e4c
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-18 22:37:18 -0500

    adding styling to resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,6 +7,7 @@ import resume from '../media/Kira_Griffitt_resume_20211031_v3.pdf';
 // return JSX for resume
 export default function Resume() {
   return (
+
     // resume section
     <div className="container resume-section">
 
@@ -16,26 +17,27 @@ export default function Resume() {
         Download my resume <a className="resume-link" target="blank" href={resume}>here</a>.
       </p>
 
-      {/* proficiencies */}
-      <h3>Front-end Proficiencies</h3>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Javascript</li>
-        <li>responsive design</li>
-        <li>Bootstrap, Materialize, React Bootstrap</li>
-        <li>React</li>
-      </ul>
+      {/* front-end proficiencies */}
+      <h3 className="frontend-heading">Front-End Proficiencies</h3>
+        <ul className="frontend-list">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>responsive design</li>
+          <li>Bootstrap, Materialize, React Bootstrap</li>
+          <li>React</li>
+        </ul>
 
-      <h3>Back-end Proficiencies</h3>
-      <ul>
-        <li>APIs</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>SQL, Sequelize</li>
-        <li>MongoDB, Mongoose</li>
-        <li>REST</li>
-      </ul>
+      {/* back-end proficiencies */}
+      <h3 className="backend-heading">Back-End Proficiencies</h3>
+        <ul className="backend-list">
+          <li>APIs</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>SQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+          <li>REST</li>
+        </ul>
 
     </div>
   );
