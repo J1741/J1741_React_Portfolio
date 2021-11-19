32b32860b3f4de12d7e0d4ecc63b5d61f6605252
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-19 05:11:35 -0500

    testing rendering Project JSX directly in Portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,9 +2,35 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Portfolio.css';
 import Project from './Project';
+import beverageViberGif from '../media/beverage_viber.gif'
 
 export default function Portfolio() {
   return(
-    <Project />
+    <>
+    <h3 className="portfolio-heading">Portfolio</h3>
+    <div className="row row-cols-1 row-cols-md-2 g-4 app-gallery"></div>
+
+      {/* Beverage Viber */}
+      <div className="col">
+        <div className="card featured-app">
+          <img
+            src={beverageViberGif}
+            className="card-img"
+          />
+          <div className="app-overlay">
+            <h5 className="card-title">
+              <a href="https://guarded-meadow-10732.herokuapp.com/">The Beverage Viber</a>
+            </h5>
+            <p className="card-text"><a href="https://github.com/shobes572/The_Beverage_Viber">GitHub</a></p>
+          </div>
+        </div>
+      </div>
+
+      {/* Vibe Cloud */}
+      {/* E-Commerce Backend */}
+      {/* Team Profile Generator */}
+      {/* Password Generator */}
+      {/* Weather Dashboard */}
+    </>
   )
 } 
