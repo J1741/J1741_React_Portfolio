995d984eb0d9df9faca5195d6f2341666ab00b38
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-19 05:27:09 -0500

    refactoring Project.js to single component, with placeholders for props values

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,110 +2,29 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Project.css';
 
-export default function Project () {
+export default function Project (props) {
   return (
     <>
-    {/* TESTING - MOVE?? */}
-    <h3 className="portfolio-heading">Portfolio</h3>
-    <div className="row row-cols-1 row-cols-md-2 g-4 app-gallery">
-
-      {/* TESTING App-1 */}
-      <div className="col">
+    
+     {/* App-X */}
+     <div className="col">
         <div className="card featured-app">
+          {/* pass app image src from props */}
           <img
-            src="https://placekitten.com/g/500/300"
+            src={}
             className="card-img"
           />
           <div className="app-overlay">
             <h5 className="card-title">
-              <a href="https://placekitten.com/g/500/300">Card title</a>
+              {/* pass deployed url and app title from props */}
+              <a href={}>{}</a>
             </h5>
-            <p className="card-text"><a href="http://www.npr.org">GitHub</a></p>
+            {/* pass github repo url from props */}
+            <p className="card-text"><a href={}>GitHub</a></p>
           </div>
         </div>
-      </div>
+      </div> 
 
-      {/* TESTING App-2 */}
-      <div className="col">
-        <div className="card featured-app">
-          <img
-            src="https://placekitten.com/g/500/300"
-            className="card-img"
-          />
-          <div className="app-overlay">
-            <h5 className="card-title">
-              <a href="https://placekitten.com/g/500/300">Card title</a>
-            </h5>
-            <p className="card-text"><a href="http://www.npr.org">GitHub</a></p>
-          </div>
-        </div>
-      </div>
-
-      {/* TESTING App-3 */}
-      <div className="col">
-        <div className="card featured-app">
-          <img
-            src="https://placekitten.com/g/500/300"
-            className="card-img"
-          />
-          <div className="app-overlay">
-            <h5 className="card-title">
-              <a href="https://placekitten.com/g/500/300">Card title</a>
-            </h5>
-            <p className="card-text"><a href="http://www.npr.org">GitHub</a></p>
-          </div>
-        </div>
-      </div>
-
-      {/* TESTING App-4 */}
-      <div className="col">
-        <div className="card featured-app">
-          <img
-            src="https://placekitten.com/g/500/300"
-            className="card-img"
-          />
-          <div className="app-overlay">
-            <h5 className="card-title">
-              <a href="https://placekitten.com/g/500/300">Card title</a>
-            </h5>
-            <p className="card-text"><a href="http://www.npr.org">GitHub</a></p>
-          </div>
-        </div>
-      </div>
-
-      {/* TESTING App-5 */}
-      <div className="col">
-        <div className="card featured-app">
-          <img
-            src="https://placekitten.com/g/500/300"
-            className="card-img"
-          />
-          <div className="app-overlay">
-            <h5 className="card-title">
-              <a href="https://placekitten.com/g/500/300">Card title</a>
-            </h5>
-            <p className="card-text"><a href="http://www.npr.org">GitHub</a></p>
-          </div>
-        </div>
-      </div>
-
-      {/* TESTING App-6 */}
-      <div className="col">
-        <div className="card featured-app">
-          <img
-            src="https://placekitten.com/g/500/300"
-            className="card-img"
-          />
-          <div className="app-overlay">
-            <h5 className="card-title">
-              <a href="https://placekitten.com/g/500/300">Card title</a>
-            </h5>
-            <p className="card-text"><a href="http://www.npr.org">GitHub</a></p>
-          </div>
-        </div>
-      </div>
-
-    </div>
     </>
   );
 }
