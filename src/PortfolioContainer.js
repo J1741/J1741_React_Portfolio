9ff2e1be3eea7637e8b7e34211699a8122295eae
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-19 00:18:14 -0500

    adding rest of renderPage logic and needed imports

--- src/PortfolioContainer.js
+++ src/PortfolioContainer.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/PortfolioContainer.css';
 import AboutMe from './components/AboutMe';
-import Header from './components/Header';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Header from './components/Header';
+import Project from './components/Project';
+import Resume from './components/Resume';
 
 // renders portfolio container
 export default function PortfolioContainer() {
@@ -17,9 +20,18 @@ export default function PortfolioContainer() {
     if (currentPage === 'AboutMe') {
       return <AboutMe />;
     }
-    // ** if currentPage is Portfolio, return Portfolio
+    // ** if currentPage is Portfolio, return ??Project component for each project??
+    if (currentPage === 'Portfolio') {
+      return <Project />;
+    }
     // ** if currentPage is Contact, return Contact
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
     // ** if currentPage is Resume, return Resume
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
     // otherwise, return AboutMe
     return <AboutMe />;
   }
