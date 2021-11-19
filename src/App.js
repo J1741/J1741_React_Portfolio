a792d738753d5bb001c447bdd5a9608bce2ca12d
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-18 23:54:14 -0500

    moving rendering control from App.js to PortfolioContainer.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
-import AboutMe from './components/AboutMe';
-import Contact from './components/Contact'
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Project from './components/Project';
-import Resume from './components/Resume';
+// import AboutMe from './components/AboutMe';
+// import Contact from './components/Contact'
+// import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Project from './components/Project';
+// import Resume from './components/Resume';
+import PortfolioContainer from './PortfolioContainer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       {/* <AboutMe /> */}
       {/* <Contact /> */}
       {/* <Resume /> */}
-      <Project />
-      <Footer />
+      {/* <Project /> */}
+      {/* <Footer /> */}
+      <PortfolioContainer />
     </div>
   );
 }
---
+++ src/PortfolioContainer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/PortfolioContainer.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default function PortfolioContainer() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
