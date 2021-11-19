35f4400f6cb3e3ecac470229332a55daa4bcccd9
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-19 05:01:04 -0500

    cleaning up comments in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
-// import AboutMe from './components/AboutMe';
-// import Contact from './components/Contact'
-// import Footer from './components/Footer';
-// import Header from './components/Header';
-// import Project from './components/Project';
-// import Resume from './components/Resume';
 import PortfolioContainer from './PortfolioContainer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      {/* <AboutMe /> */}
-      {/* <Contact /> */}
-      {/* <Resume /> */}
-      {/* <Project /> */}
-      {/* <Footer /> */}
       <PortfolioContainer />
     </div>
   );
